64139673a408a22f7925ce2241c47d810d605ab2
Jaraslau Zaitsau <122669697+jaraslauzaitsau@users.noreply.github.com>
2026-08-19 22:07:44 +0200

    Add exit button

--- src/ui/pages/MainPage.cpp
+++ src/ui/pages/MainPage.cpp
@@ -1,11 +1,17 @@
 #include "ui/pages/MainPage.hpp"
 #include "ui/widgets/Label.hpp"
-#include "ui/widgets/Widget.hpp"
+#include "ui/widgets/Button.hpp"
 #include <raylib.h>
 #include <raygui.h>
 
 MainPage::MainPage() {
     grid.set_widget({0, 0}, new Label("Main Page"), SizePolicy{SizePolicyValue::Expand, SizePolicyValue::Expand});
+    Button* exit = new Button("Exit");
+    exit->set_callback([] () {
+    	CloseWindow();
+     	std::exit(0);
+    });
+    grid.set_widget({1, 1}, exit);
 }
 
 MainPage::~MainPage() {
